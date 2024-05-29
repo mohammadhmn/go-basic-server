d4b33e057f7df79b2bcd7d68f20b386699fcb1db
Mohamadhosein Moazennia <mhmoazennia@gmail.com>
2024-05-29 21:53:44 +0330

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -33,12 +33,15 @@ func main() {
 		fmt.Println("Error in reading connection: ", err.Error())
 		os.Exit(1)
 	}
-	if strings.HasPrefix(string(buffer), "GET / HTTP/1.1") {
+	stringBuffer := string(buffer)
+	if strings.HasPrefix(stringBuffer, "GET / HTTP/1.1") {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.HasPrefix(stringBuffer, "GET /echo/") {
+		statusLine := strings.Split(stringBuffer, "\r\n")[0]
+		endpoint := strings.Split(statusLine, " ")[1]
+		echo := strings.Split(endpoint, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	fmt.Println(string(buffer))
-
-	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
